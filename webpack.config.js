caa5d965d5c5cf97f22fcbc3cb5db697c0fb5bdf
Peter Lopez <peter@peterlopez.me>
2020-01-01 08:38:14 -0800

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
